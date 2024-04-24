ee594bfe6a971dabf2c231021f1ff2af486a8563
Ojin Timo <89027644+OjinTimo@users.noreply.github.com>
2024-04-24 09:08:08 +0200

    added firefly to the background

--- src/app/components/navigation/NavButton.jsx
+++ src/app/components/navigation/NavButton.jsx
@@ -31,19 +31,19 @@ const NavButton = ({ x, y, label, link, icon, newTab }) => {
         <div className='absolute cursor-pointer z-50'
             style={{ transform: `translate(${x}, ${y})` }}
         >
-            <Link 
-            href={link} 
-            target={newTab ? "_blank" : "_self"}
-            className='text-foreground rounded-full items-center flex justify-center 
-            bg-background/20 border border-accent/30 border-solid backdrop-blur-[6px] shadow-glass-inset hover:shadow-glass-sm' 
-            name={label} 
-            aria-label={label}>
+            <Link
+                href={link}
+                target={newTab ? "_blank" : "_self"}
+                className='text-foreground rounded-full items-center flex justify-center 
+            bg-background/20 border border-accent/30 border-solid backdrop-blur-[6px] shadow-glass-inset hover:shadow-glass-sm'
+                name={label}
+                aria-label={label}>
                 <span className='relative w-14 h-14 p-4 animate-spin-slow-reverse group-hover:pause hover:text-accent'>
                     {getIcon(icon)}
                     <span className='peer bg-transparent absolute top-0 left-0 w-full h-full' />
-                <span className='absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap'>
-                    {label}
-                </span>                    
+                    <span className='absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap'>
+                        {label}
+                    </span>
                 </span>
 
             </Link>
