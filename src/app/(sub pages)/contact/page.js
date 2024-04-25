9abe6d13757cf3e1925dc1752f708686f2f6948b
Ojin Timo <cryptowizard33@protonmail.com>
2024-04-25 21:47:45 +0200

    update mobile responsive

--- src/app/(sub pages)/contact/page.js
+++ src/app/(sub pages)/contact/page.js
@@ -8,12 +8,12 @@ export default function Contact() {
     <>
       <Image src={bg} alt="background-image" className='-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-25' />
 
-      <article className='relative w-full flex flex-col items-center justify-center space-y-8'>
-        <div className='flex flex-col items-center justify-center space-y-6 w-3/4'>
+      <article className='relative w-full flex flex-col items-center justify-center py-8 sm:py-0 space-y-8'>
+        <div className='flex flex-col items-center justify-center space-y-6 w-full sm:w-3/4'>
           <h1 className='text-accent font-semibold text-center text-4xl capitalize'>
             Summon the chef
           </h1>
-          <p className='text-center font-light'>
+          <p className='text-center font-light text-sm xs:text-base'>
           Welcome to Crunchy Cheddar Studio, where creativity meets passion! If you're seeking top-notch game or web development services, look no further. 
           Let's collaborate and bring your vision to life, pixel by pixel or code by code. 
           Drop me a line below and let's make magic happen together.
--- src/app/(sub pages)/projects/page.js
+++ src/app/(sub pages)/projects/page.js
@@ -11,7 +11,7 @@ export default function page() {
     <>
         <Image src={bg} alt="background-image"  className='-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-25' />
         <ProjectList projects={projectsData} />
-        <div className='flex items-center justify-center fixed top-20 -left-5 h-screen'>
+        <div className='flex items-center justify-center fixed top-16 lg:top-20 left-1/2 -translate-x-1/2 lg:translate-x-0 -z-10 lg:-left-5 h-screen'>
         <RenderModel>
             <CheeseAvatar />
         </RenderModel>            
