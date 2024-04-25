41dcf960c8535faf572685d96e384ae6d7890759
Ojin Timo <cryptowizard33@protonmail.com>
2024-04-25 18:41:12 +0200

    updating mobile responsive

--- src/app/components/about/index.jsx
+++ src/app/components/about/index.jsx
@@ -2,7 +2,7 @@ import clsx from 'clsx'
 import React from 'react'
 
 const ItemLayout = ({ children, className }) => {
-    return <div className={clsx('custom-bg p-8 rounded-xl flex items-center justify-center space-y-8', className)}>
+    return <div className={clsx('custom-bg sm:p-8 p-6 rounded-xl flex items-center justify-center space-y-8', className)}>
         {children}
     </div>
 }
@@ -10,12 +10,12 @@ const ItemLayout = ({ children, className }) => {
 const AboutDetails = () => {
     return (
         <section className='py-20 w-full'>
-            <div className='grid grid-cols-12 gap-8 w-full'>
-                <ItemLayout className={'col-span-8 row-span-2 flex-col items-start'}>
-                    <h2 className='text-2xl text-left w-full capitalize text-accent'>
+            <div className='grid grid-cols-12 gap-4 xs:gap-6 md:gap-8 w-full'>
+                <ItemLayout className={'col-span-full lg:col-span-8 row-span-2 flex-col items-start'}>
+                    <h2 className='text-xl md:text-2xl text-left w-full capitalize text-accent'>
                         Master Chef of the kitchen, elevate the cheddar to perfection.
                     </h2>
-                    <p className='font-light'>
+                    <p className='font-light text-xs sm:text-sm md:text-base'>
                         In the bustling kitchen of Crunchy Cheddar Studio, my journey in web development unfolds like a delectable recipe, 
                         powered by an array of sharp tools and languages. 
                         With JavaScript as the melting core of my cooking, I deftly wield frameworks like React.js and Next.js, 
@@ -26,17 +26,17 @@ const AboutDetails = () => {
                         Join me, fellow connoisseurs, as I continue to explore new recipes and technologies, shaping the future of the web and gaming landscape with a dash of cheesy delight.
                     </p>
                 </ItemLayout>
-                <ItemLayout className={'col-span-4 text-accent'}>
-                    <p className='font-semibold w-full text-left text-5xl'>
+                <ItemLayout className={'col-span-full xs:col-span-6 lg:col-span-4 text-accent'}>
+                    <p className='font-semibold w-full text-left text-2xl sm:text-5xl'>
                         5+ <sub className='font-semibold text-base'>clients</sub>
                     </p>
                 </ItemLayout>
-                <ItemLayout className={'col-span-4 text-accent'}>
-                    <p className='font-semibold w-full text-left text-5xl'>
+                <ItemLayout className={'col-span-full xs:col-span-6 lg:col-span-4 text-accent'}>
+                    <p className='font-semibold w-full text-left text-2xl sm:text-5xl'>
                         3+ <sub className='font-semibold text-base'>years of experience</sub>
                     </p>
                 </ItemLayout>
-                <ItemLayout className={"col-span-4 !p-0"}>
+                <ItemLayout className={"col-span-full sm:col-span-6 md:col-span-4 !p-0"}>
                     <img className='w-full h-auto' src="https://github-readme-stats.vercel.app/api/top-langs?username=crunchycheddarstudio&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false" alt="crunchycheddarstudio" loading='lazy' />
                 </ItemLayout>
                 <ItemLayout className={"col-span-8 p-0"}>
