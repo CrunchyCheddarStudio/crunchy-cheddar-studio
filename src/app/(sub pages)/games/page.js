f92969fae23798c96c941c139c06e2c051b787c1
Ojin Timo <cryptowizard33@protonmail.com>
2024-04-25 18:59:01 +0200

    game page

---
+++ src/app/(sub pages)/games/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import bg from "../../../../public/background/games-background.png"
+import Image from 'next/image'
+
+export default function page() {
+  return (
+    <>
+      <Image src={bg} alt="background-image" className='-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-25' />
+
+    </>
+  )
+}
+
